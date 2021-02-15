b6db78c435717acccf85980d2357db266748ca40
vinicio oliveira <um.vinicio@gmail.com>
2021-02-15 16:26:30 +0100

    horoscope#show methods

--- app/controllers/horoscopes_controller.rb
+++ app/controllers/horoscopes_controller.rb
@@ -4,5 +4,18 @@ class HoroscopesController < ApplicationController
   end
   
   def show
+    @horoscope = Horoscope.find(params[:id])
+
+    @quotes = []
+    unless @horoscope.id == 6
+      3.times do
+        @quotes << Faker::Movies::StarWars.unique.quote(character: @horoscope.name) 
+      end
+    else
+      3.times do
+        @quotes << Faker::Movies::StarWars.unique.wookiee_sentence
+      end
+    end
+    Faker::Movies::StarWars.unique.clear
   end
 end
